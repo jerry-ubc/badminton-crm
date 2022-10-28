d96fca844f1dd603a939156ba64c0a881d57c25a
jerry <jerry.jge@gmail.com>
2022-10-28 11:48:24 +0800

    enforced unique usernames

--- signup.php
+++ signup.php
@@ -1,66 +1,28 @@
-<?php
-// session_start();
-//     include("connection.php");
-//     include("functions.php");
-
-//     if($_SERVER['REQUEST_METHOD'] == "POST")
-//     {
-//         #something was posted, so collect data from post variable
-//         $user_name = $_POST['user_name'];
-//         $password = $_POST['password'];
-
-//         if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-//         {
-//             #save to database
-//             $user_id = random_num(20);
-//             $query = "insert into users (user_id, user_name, password) values ('$user_id', '$user_name', '$password')";
-//             #id and date will be created by MySQL, not us
-//             mysqli_query($con, $query);
-//             header("Location: login.php");      #created account, now login
-//             die;
-//         }
-//         else
-//         {
-//             echo "Please FOLLOW THE RULES THAT I'LL POST LATER";
-//         }
-//     }
-?>
-<!-- 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Signup</title>
-</head>
-<body>
-    <div id = "box">
-        <form method = "post">
-            <div>Signup</div>
-            <input type = "text" name = "user_name"><br><br>
-            <input type = "password" name = "password"><br><br>
-            <input type = "submit" value = "Signup"><br><br>
-
-            <a href = "login.php">Already have an account?</a><br><br>
-        </form>
-    </div>
-</body>
-</html> -->
-
 <?php 
 session_start();
-
 	include("connection.php");
 	include("functions.php");
-
 
 	if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
 		//something was posted
 		$user_name = $_POST['user_name'];
 		$password = $_POST['password'];
+        $valid = TRUE;
+        if(empty($user_name || empty($password)))
+        {
+            echo "Username or password can't be empty";
+            $valid = FALSE;
+        }
+        $query = "select * from users where user_name = '$user_name' limit 1";
+        $result = mysqli_query($con, $query);
+        if(mysqli_num_rows($result) > 0 && $valid) {
+            echo "Username already exists";
+            $valid = FALSE;
+        }
 
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		if($valid)
 		{
-
 			//save to database
 			$user_id = random_num(20);
 			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
@@ -69,10 +31,11 @@ session_start();
 
 			header("Location: login.php");
 			die;
-		}else
-		{
-			echo "Please enter some valid information!";
 		}
+        // else
+		// {
+		// 	echo "WEIRD INPUT DETECTED!!!!!";
+		// }
 	}
 ?>
 
